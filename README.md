997c4f310ccfae62072aae809655042d4a7c48dd
sue7ga <sue77ga@gmail.com>
2014-04-21 08:48:12 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Kosoku::JP::API - It's new $module
+
+# SYNOPSIS
+
+    use Kosoku::JP::API;
+
+# DESCRIPTION
+
+Kosoku::JP::API is ...
+
+# LICENSE
+
+Copyright (C) sue7ga.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sue7ga <sue77ga@gmail.com>
---
+++ lib/Kosoku/JP/API.pm
@@ -0,0 +1,39 @@
+package Kosoku::JP::API;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Kosoku::JP::API - It's new $module
+
+=head1 SYNOPSIS
+
+    use Kosoku::JP::API;
+
+=head1 DESCRIPTION
+
+Kosoku::JP::API is ...
+
+=head1 LICENSE
+
+Copyright (C) sue7ga.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sue7ga E<lt>sue77ga@gmail.comE<gt>
+
+=cut
+
